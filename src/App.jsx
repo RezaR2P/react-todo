4594bbe233b6ade1e7e2907b4375d6adaf873afa
“RezaR2P” <kecilreza575@gmail.com>
2024-06-14 23:52:09 +0700

    mencoba memperbaiki bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// src/App.js
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Welcome from "./pages/Welcome";
 import Goodbye from "./pages/Goodbye";
@@ -6,11 +8,12 @@ import Goodbye from "./pages/Goodbye";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/goodbye" element={<Goodbye />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/home" component={Home} />
+        <Route path="/goodbye" component={Goodbye} />
+        <Route component={() => <h1>404: Page Not Found</h1>} />
+      </Switch>
     </Router>
   );
 }
