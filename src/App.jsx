a8ffef115f3329d30dea3c3561752d50974ea93a
“RezaR2P” <kecilreza575@gmail.com>
2024-06-14 23:54:42 +0700

    memperbaiki ke2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// src/App.js
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Welcome from "./pages/Welcome";
 import Goodbye from "./pages/Goodbye";
@@ -8,12 +6,12 @@ import Goodbye from "./pages/Goodbye";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route path="/home" component={Home} />
-        <Route path="/goodbye" component={Goodbye} />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/goodbye" element={<Goodbye />} />
         <Route component={() => <h1>404: Page Not Found</h1>} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
